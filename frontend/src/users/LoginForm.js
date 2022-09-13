178a15564136f0689cd6cda22eb956b4287ac76d
John Weilert <mattweilert@gmail.com>
2022-09-12 19:34:11 -0500

    jwt

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -15,7 +15,6 @@ function LoginForm() {
   const [errorMessage, setErrorMessage] = useState(null);
 
   async function handleSubmit(e) {
-    e.preventDefault();
     const response = await fetch(`http://localhost:5000/authentication/`, {
       method: "POST",
       headers: {
@@ -28,55 +27,55 @@ function LoginForm() {
 
     if (response.status === 200) {
       setCurrentUser(data.user);
-      history.push("/");
+      localStorage.setItem("token", data.token);
+      history.push(`/`);
     } else {
       setErrorMessage(data.message);
     }
+
+    return (
+      <main>
+        <h1>Login</h1>
+        {errorMessage !== null ? (
+          <div className="alert alert-danger" role="alert">
+            {errorMessage}
+          </div>
+        ) : null}
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-sm-6 form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                required
+                value={credentials.email}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, email: e.target.value })
+                }
+                className="form-control"
+                id="email"
+                name="email"
+              />
+            </div>
+            <div className="col-sm-6 form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                required
+                value={credentials.password}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, password: e.target.value })
+                }
+                className="form-control"
+                id="password"
+                name="password"
+              />
+            </div>
+          </div>
+          <input className="btn btn-primary" type="submit" value="Login" />
+        </form>
+      </main>
+    );
   }
-
-  return (
-    <main>
-      <h1>Login</h1>
-      {errorMessage !== null ? (
-        <div className="alert alert-danger" role="alert">
-          {errorMessage}
-        </div>
-      ) : null}
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-sm-6 form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              required
-              value={credentials.email}
-              onChange={(e) =>
-                setCredentials({ ...credentials, email: e.target.value })
-              }
-              className="form-control"
-              id="email"
-              name="email"
-            />
-          </div>
-          <div className="col-sm-6 form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              required
-              value={credentials.password}
-              onChange={(e) =>
-                setCredentials({ ...credentials, password: e.target.value })
-              }
-              className="form-control"
-              id="password"
-              name="password"
-            />
-          </div>
-        </div>
-        <input className="btn btn-primary" type="submit" value="Login" />
-      </form>
-    </main>
-  );
 }
-
 export default LoginForm;
