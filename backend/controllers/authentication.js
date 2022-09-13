bb8bb16e04d04a871ddc3ae44a86287fb378733e
John Weilert <mattweilert@gmail.com>
2022-09-12 20:45:43 -0500

    Changed packages

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({
     where: { email: req.body.email },
   });
-  ___;
+
   if (
     !user ||
     !(await bcrypt.compare(req.body.password, user.passwordDigest))
